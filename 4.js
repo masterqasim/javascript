0258c5412f31f9e06fe7f65bfc327a9ff8833d4b
Muhammad Qasim <39057096+masterqasim@users.noreply.github.com>
2024-01-05 23:12:10 +0500

    add solution

--- 4.js
+++ 4.js
@@ -34,3 +34,33 @@ Constraints:
 -1000 <= init <= 1000
 0 <= calls.length <= 1000
 calls[i] is one of "increment", "decrement" and "reset"
+
+solution:
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let current = init ;
+    return {
+        increment: ()=>{
+            current ++
+            return current
+        },
+        decrement: ()=>{
+            current --
+            return current
+        },
+        reset:()=>{
+            current = init
+            return current
+        }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
