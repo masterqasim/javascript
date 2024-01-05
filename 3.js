418380966ed1de2b5b392383fc381cde7c41161e
Muhammad Qasim <39057096+masterqasim@users.noreply.github.com>
2024-01-05 22:48:09 +0500

    add solution

--- 3.js
+++ 3.js
@@ -22,3 +22,32 @@ Example 3:
 Input: func = () => expect(5).notToBe(null)
 Output: {"value": true}
 Explanation: 5 !== null so this expression returns true.
+
+ Solution:
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe:function(val1){
+            if (val === val1){
+                return true
+            } else {
+                throw new Error("Not Equal")
+            }
+        },
+        notToBe:function(val2){
+            if(val !== val2){
+                return true
+            }else{
+                throw new Error("Equal")    
+            }
+        }
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
