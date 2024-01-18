c163c519791e56ec0c3e3947b34669b9661acac3
Muhammad Qasim <39057096+masterqasim@users.noreply.github.com>
2024-01-18 23:35:11 +0500

    add solution

--- 14.js
+++ 14.js
@@ -51,3 +51,45 @@ args is a valid JSON array
 1 <= args.length <= 10
 20 <= t <= 1000
 10 <= cancelTimeMs <= 1000
+
+solutions:
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function(fn, args, t) {
+    let timeoutId;
+    timeoutId = setTimeout(() => {
+        const result = fn(...args); 
+    
+    }, t);
+    return function(){
+       clearTimeout(timeoutId);
+    }
+};
+
+/**
+ *  const result = [];
+ *
+ *  const fn = (x) => x * 5;
+ *  const args = [2], t = 20, cancelTimeMs = 50;
+ *
+ *  const start = performance.now();
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start);
+ *      result.push({"time": diff, "returned": fn(...argsArr)});
+ *  }
+ *       
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  const maxT = Math.max(t, cancelTimeMs);
+ *           
+ *  setTimeout(cancel, cancelTimeMs);
+ *
+ *  setTimeout(() => {
+ *      console.log(result); // [{"time":20,"returned":10}]
+ *  }, maxT + 15)
+ */
