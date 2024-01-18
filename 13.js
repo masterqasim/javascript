cfd2520a681e6d3e33525eb2d55167cf94bd527f
Muhammad Qasim <39057096+masterqasim@users.noreply.github.com>
2024-01-18 16:17:49 +0500

    add solution

--- 13.js
+++ 13.js
@@ -23,3 +23,21 @@ Explanation: It should return a promise that resolves after 200ms.
 Constraints:
 
 1 <= millis <= 1000
+
+solution:
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(millis);
+    }, millis);
+  });
+}
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
