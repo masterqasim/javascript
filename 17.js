3f2622b86b9a5a0c9f88651d2b526b1d22d8266b
Muhammad Qasim <39057096+masterqasim@users.noreply.github.com>
2024-01-28 23:13:15 +0500

    added solution

--- 17.js
+++ 17.js
@@ -56,3 +56,51 @@ actions.length === timeDelays.length
 0 <= timeDelays[i] <= 1450
 actions[i] is one of "TimeLimitedCache", "set", "get" and "count"
 First action is always "TimeLimitedCache" and must be executed immediately, with a 0-millisecond delay
+
+solutions:
+var TimeLimitedCache = function() {
+    this.cache = {};
+    this.activeKeysCounter = 0;
+};
+
+/** 
+ * @param {number} key
+ * @param {number} value
+ * @param {number} duration time until expiration in ms
+ * @return {boolean} if un-expired key already existed
+ */
+TimeLimitedCache.prototype.set = function(key, value, duration) {
+    var keyInCache = this.cache[key];
+    keyInCache ? clearTimeout(keyInCache.timeout) : this.activeKeysCounter++;
+    
+    this.cache[key] = {
+      value: value,
+      timeout: setTimeout(() => {
+        this.cache[key].value = -1;
+        this.activeKeysCounter--;
+      }, duration),
+    }
+    return Boolean(keyInCache);
+};
+
+/** 
+ * @param {number} key
+ * @return {number} value associated with key
+ */
+TimeLimitedCache.prototype.get = function(key) {
+    return this.cache[key]?.value || -1;
+};
+
+/** 
+ * @return {number} count of non-expired keys
+ */
+TimeLimitedCache.prototype.count = function() {
+    return this.activeKeysCounter;
+};
+
+/**
+ * const timeLimitedCache = new TimeLimitedCache()
+ * timeLimitedCache.set(1, 42, 1000); // false
+ * timeLimitedCache.get(1) // 42
+ * timeLimitedCache.count() // 1
+ */
